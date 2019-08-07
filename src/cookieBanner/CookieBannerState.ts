3f696bc72e346dd522603c69d8fa22786b022fc9
Alexandru Ciuca <alexandru.ciuca@consensys.net>
2019-08-07 13:24:38 +0300

    Add cookie banner

---
+++ src/cookieBanner/CookieBannerState.ts
@@ -0,0 +1,21 @@
+import { observable } from "mobx";
+import { IStorage } from "./IStorage";
+
+export class CookieBannerState {
+    @observable
+    private _cookiesAccepted: boolean;
+
+    constructor(private storage: IStorage, private storageKey = "cookiesConsent") {
+        let storedData = storage.getItem(this.storageKey) || "false";
+        this._cookiesAccepted = ["true", "1", "on"].indexOf(storedData) !== -1;
+    }
+
+    get cookiesAccepted() {
+        return this._cookiesAccepted;
+    }
+
+    set cookiesAccepted(v: boolean) {
+        this.storage.setItem(this.storageKey, v ? "true" : "false");
+        this._cookiesAccepted = v;
+    }
+}
